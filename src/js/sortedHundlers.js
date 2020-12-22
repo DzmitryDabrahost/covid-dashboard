3ff12045c747c5362f2bc38e773d5468bac516ca
dzmitrydabrahost <dzmitrydabrahost@gmail.com>
2020-12-22 17:08:30 +0300

    fix: fix checkSorted function, now all correct

--- src/js/sortedHundlers.js
+++ src/js/sortedHundlers.js
@@ -8,13 +8,17 @@ export default function sortedHundlers(data) {
 }
 
 function viewSorted(data, target) {
+  const toggle = document.querySelector('.toggle-last-day-item').classList.contains('item-on');
   if (target.matches('.sortCountries__var--yellow')) {
-    getDataByCountry(data.sort((a, b) => b.cases - a.cases));
+    if (toggle) getDataByCountry(data.sort((a, b) => b.todayCases - a.todayCases));
+    else getDataByCountry(data.sort((a, b) => b.cases - a.cases));
   }
   if (target.matches('.sortCountries__var--blueGrey')) {
-    getDataByCountry(data.sort((a, b) => b.deaths - a.deaths));
+    if (toggle) getDataByCountry(data.sort((a, b) => b.todayDeaths - a.todayDeaths));
+    else getDataByCountry(data.sort((a, b) => b.deaths - a.deaths));
   }
   if (target.matches('.sortCountries__var--green')) {
-    getDataByCountry(data.sort((a, b) => b.recovered - a.recovered));
+    if (toggle) getDataByCountry(data.sort((a, b) => b.todayRecovered - a.todayRecovered));
+    else getDataByCountry(data.sort((a, b) => b.recovered - a.recovered));
   }
 }
